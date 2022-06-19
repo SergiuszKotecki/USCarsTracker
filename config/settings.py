ea8000ff5569f7f728a9b0cec8b898510f58ad34
KoteckiS <sergiusz.kotecki@trw.com>
2022-06-19 15:22:40 +0200

    Add base auth user model, add accounts apps to installed apps

--- config/settings.py
+++ config/settings.py
@@ -37,7 +37,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # local
+    'accounts.apps.AccountsConfig',
 ]
+
+AUTH_USER_MODEL = 'accounts.CustomUser'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
