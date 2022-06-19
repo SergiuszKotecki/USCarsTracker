c86ee6228d5caf5db2c119089ae9698a4b7f6627
KoteckiS <sergiusz.kotecki@trw.com>
2022-06-19 16:16:29 +0200

    minor fixes

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 
-# Register your models here.
+from accounts.forms import CustomUserChangeForm
+from accounts.forms import CustomUserCreationForm
+from accounts.models import CustomUser
+
+
+class CustomUserAdmin(UserAdmin):
+    add_form = CustomUserCreationForm
+    form = CustomUserChangeForm
+    model = CustomUser
+    list_display = ['username', 'email', 'premium', 'is_staff', ]
+    fieldsets = UserAdmin.fieldsets + (
+        (None, {'fields': ('premium',)}),
+    )
+    add_fieldsets = UserAdmin.add_fieldsets + (
+        (None, {'fields': ('premium',)}),
+    )
+
+
+admin.site.register(CustomUser, CustomUserAdmin)
