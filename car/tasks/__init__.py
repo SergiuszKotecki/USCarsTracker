32e67eef7460fdacd58bfe974ab46bc384270d86
KoteckiS <sergiusz.kotecki@trw.com>
2022-06-22 09:26:17 +0200

    Install requests dependency

---
+++ car/tasks/__init__.py
@@ -0,0 +1,13 @@
+# -*- encoding: utf-8 -*-
+# ----------------------------------------------------------------------------------------------------------------------
+# Author:  KOTECKIS
+# E-mail:  sergiusz.kotecki@zf.com
+# Company: ZF Group
+# Created: 20/06/2022
+# ----------------------------------------------------------------------------------------------------------------------
+# This  copyrighted  document is the property of ZF Friedrichshafen AG and its affiliates, including ZF Group Automotive
+# Holdings Corp. and its subsidiary  companies ("ZF").  This document and the information contained  herein is disclosed
+# in  confidence  and  may  not be  copied, disclosed to  others, or  used  for any  purpose  unless  authorized  by ZF.
+# This  document must  be returned  to ZF upon request  and  may not  be  stored in any  database  or retrieval  system.
+#                                     © ZF Friedrichshafen AG 20/06/2022.
+# ----------------------------------------------------------------------------------------------------------------------
