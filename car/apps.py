3a4d75bd7eb758f8fb1b77cb35ff2484edcd7cb0
KoteckiS <sergiusz.kotecki@trw.com>
2022-06-24 13:26:00 +0200

    Setup APScheduler to run only when application is ready and ensure it runs only once

--- car/apps.py
+++ car/apps.py
@@ -1,6 +1,13 @@
 from django.apps import AppConfig
 
+from config.settings import SCHEDULER_AUTOSTART
+
 
 class CarConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'car'
+
+    def ready(self):
+        if SCHEDULER_AUTOSTART:
+            from car.tasks.test_task import start_aps
+            start_aps()
